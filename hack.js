cb75015f73efdbb90697752cea957fdfe5497250
MarkChang <mark23456@hotmail.com>
2013-01-26 17:48:33 +0800

    version 1

--- hack.js
+++ hack.js
@@ -1,15 +1,60 @@
-var template2='<button class="css3button btn1" id="input_get">recover</button>';
+var recover_button='<button class="css3button btn1" style="display:none" id="input_recover">回復</button>';
+//var clear_button='<button class="css3button btn1" style="display:none" id="input_clear">清除</button>';
+
+
 if ($("#message_typing").length > 0 ){
 	//alert("exist");
 	//$('body').prepend(template2);
-	$('#message_typing').parent().prepend(template2);
+	$('#message_typing').parent().prepend(recover_button);
 	//console.log(template2);
-	bindFunction("#message_typing");
+	//bindFunction("#message_typing");
+	$("#want_type").click(function(){
+		if(  (localStorage['dcard_msg_tp'] != '') &&  
+			(!(typeof(localStorage['dcard_msg_tp']) === 'undefined')) ){
+				$("#input_recover").show();
+		};
+		$(this).hide();
+		$("#message_typing").slideDown();
+		$("#message_submit").slideDown();
+		$("#want_profile").removeClass("btn2").addClass("btn1");
+	});
+
+
+	$(window).unload(function(){
+	  		//input_text=$('#message_typing').val();
+			localStorage.setItem('dcard_msg_tp', $('#message_typing').val());
+	});
+
+	$("#input_recover").click(function(){
+		//console.log(localStorage.dc_temp);
+		//alert(localStorage.dc_temp);
+		$('#message_typing').val(localStorage['dcard_msg_tp']);
+		localStorage.removeItem('dcard_msg_tp');
+	});
+	/*$("#input_clear").click(function(){
+		localStorage.removeItem('#message_typing');
+	});*/
 }
+
+
 else if ($("#article_content").length > 0 ){
-	//alert("exist");
-	//$('body').prepend(template2);
-	$('#article_content').parent().prepend(template2);
-	//console.log(template2);
-	bindFunction("#article_content");
+	$('#article_content').parent().prepend(recover_button);
+
+	if( (  (localStorage['dcard_art_ct'] != '')||(localStorage['dcard_tt'] != '') ) &&  
+			(!(typeof(localStorage['dcard_art_ct']) === 'undefined')) ){
+				$("#input_recover").show();
+	};
+
+	$(window).unload(function(){
+
+			localStorage.setItem('dcard_tt', $('#title').val());
+			localStorage.setItem('dcard_art_ct', $('#article_content').val());
+	});
+	$("#input_recover").click(function(){
+		$('#title').val(localStorage['dcard_tt']);
+		$('#article_content').val(localStorage['dcard_art_ct']);
+		localStorage.removeItem('dcard_tt');
+		localStorage.removeItem('dcard_art_ct');
+	});
 }
+
